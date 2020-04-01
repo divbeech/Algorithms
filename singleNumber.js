92856bf94a64044a43d885a93873f33f5331b288
divbeech <dyadav1006@gmail.com>
2020-04-01 15:12:43 -0400

    complete

--- singleNumber.js
+++ singleNumber.js
@@ -15,10 +15,5 @@ Output: 4
 */
 
 
-var singleNumber = function(nums) {
-    var result = 0;
-    for (var i=0; i<nums.length; i++){
-        result = nums[i]
-    }
-    return result
-};
+*/
+const singleNumber = nums => nums.reduce((acc, cur) => (acc ^ cur));
