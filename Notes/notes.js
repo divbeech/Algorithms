43d89e43b2a860a07ec578b4263d98ad1451e473
divbeech <dyadav1006@gmail.com>
2020-06-05 18:25:43 -0400

    notes

--- Notes/notes.js
+++ Notes/notes.js
@@ -1,39 +1,6 @@
 /*
-Big 0 Notation */
+Common Array Methods*/
 
-/******   0(N)   *******  concat ******/
-
-const array1 = ['a', 'b', 'c'];
-const array2 = ['d', 'e', 'f'];
-const array3 = array1.concat(array2);
-
-console.log(array3);
-// expected output: Array ["a", "b", "c", "d", "e", "f"]
-
-
-/*******  0(N*logN)  ***** slice *****/
-
-const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
-
-console.log(animals.slice(2));
-// expected output: Array ["camel", "duck", "elephant"]
-
-console.log(animals.slice(2, 4));
-// expected output: Array ["camel", "duck"]
-
-/*******  0(N*logN)  ***** splice *****
- * Splice method changed an array by removing or replacing existing elements &/or adding new elements in place */
-
-const months = ['Jan', 'March', 'April', 'June'];
-months.splice(1, 0, 'Feb');
-// inserts at index 1
-console.log(months);
-// expected output: Array ["Jan", "Feb", "March", "April", "June"]
-
-months.splice(4, 1, 'May');
-// replaces 1 element at index 4
-console.log(months);
-// expected output: Array ["Jan", "Feb", "March", "April", "May"]
 
 
 /*****    *******/
@@ -45,12 +12,26 @@ console.log(months);
  console.log(arr.pop());
  
  var arr2 = ["g", "r"];
- console.log(arr.concat(arr2));
+ console.log(arr.concat(arr2));/* joins the 2 arrays*/
  console.log(arr2);
 
  console.log(arr.join("!"));
 
- console.log(arr.reverse();)
+ console.log(arr.reverse());
+
+ console.log(arr.shift()); /* remove the first element and return that element*/
+ 
+ console.log(arr.unshift("p"))/* adds the element in the end */
+
+ console.log(arr.slice(1, 2))/* (start, n-1 index)*/
+
+ console.log(arr.sort())
+
+ console.log(arr.splice(2, 3, "add"))/* modify the array*(start, # of items to remove, add) */
+
+
+
+
 
 
 
