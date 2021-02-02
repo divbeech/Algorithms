d3547628b94660ecdbc5674e6b3ee987f4ac6644
divbeech <dyadav1006@gmail.com>
2021-02-02 14:27:10 -0500

    1 more

--- LeetcodeFebChallenge/index.js
+++ LeetcodeFebChallenge/index.js
@@ -11,3 +11,10 @@ var runningSum = function(nums) {
     return result
 };
 
+// another solution
+var runningSum = function(nums) {
+    for (let i = 1; i < nums.length; i++){
+        nums[i] += nums[i-1];
+    }
+    return nums;
+};
