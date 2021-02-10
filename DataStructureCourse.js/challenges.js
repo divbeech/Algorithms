8fe11a906e95aed13ae176a320b8071ce9c23454
divbeech <dyadav1006@gmail.com>
2021-02-10 11:14:40 -0500

    array

---
+++ DataStructureCourse.js/challenges.js
@@ -0,0 +1,9 @@
+
+const nemo = ["nemo"];
+function findNemo(array){
+    for (let x =0;x<array.length; x++){
+        if (array[x]==='nemo');
+        console.log('Found Nemo');
+    }
+}
+findNemo(nemo);
