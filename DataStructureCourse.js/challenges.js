cfe68443df07543414e56e4f825ada10b38a74dc
divbeech <dyadav1006@gmail.com>
2021-03-14 14:49:17 -0400

    boxes

--- DataStructureCourse.js/challenges.js
+++ DataStructureCourse.js/challenges.js
@@ -1,22 +1,33 @@
 
-const nemo = ["nemo"];
-function findNemo(array){
-    for (let x =0;x<array.length; x++){
-        if (array[x]==='nemo');
-        console.log('Found Nemo');
-    }
-}
-findNemo(nemo);
+// const nemo = ["nemo"];
+// function findNemo(array){
+//     for (let x =0;x<array.length; x++){
+//         if (array[x]==='nemo');
+//         console.log('Found Nemo');
+//     }
+// }
+// findNemo(nemo);
 
-// Time complexity 
- function funChallenge(input){
-     let a = 10;
-     a = 50 + 3;
+// // Time complexity 
+//  function funChallenge(input){
+//      let a = 10;
+//      a = 50 + 3;
 
-     for (let i = 0; i<input.lengthl; i++){
-         anotherFunction();
-         let stranger = true;
-         a++;
-     }
-     return a;
- } // O(1) + O(n)
+//      for (let i = 0; i<input.lengthl; i++){
+//          anotherFunction();
+//          let stranger = true;
+//          a++;
+//      }
+//      return a;
+//  } // O(1) + O(n)
+
+ function compressBoxesTwice(boxes){
+     boxes.forEach(function(boxes){
+         console.log(boxes);
+     });
+     boxes.forEach(function(boxes){
+         console.log(boxes);
+     });
+ }
+
+ // O(n)
