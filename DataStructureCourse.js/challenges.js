72dc0003b36004c7d510a88c6aed422242adf3c8
divbeech <dyadav1006@gmail.com>
2021-03-14 15:34:13 -0400

    add

--- DataStructureCourse.js/challenges.js
+++ DataStructureCourse.js/challenges.js
@@ -33,11 +33,11 @@
  // O(n)
 
  // Log all pairs of array - Nested loop is O(n^2)
- const boxes = ["a", "b", "c", "d", "e"];
+ const boxes = [1,2, 3];
  function pairs(array){
      for (let i = 0; i < array.length; i++){
          for (let j = 0; j <array.length; j++){
-             console.log(array[i] , array[j])
+             console.log(array[i] + array[j])
          }
      }
 
