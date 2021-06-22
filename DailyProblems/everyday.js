66e172650019e6e6d373e8f27df2e9cd42ceae62
divbeech <dyadav1006@gmail.com>
2021-06-22 12:14:36 -0400

    DS

---
+++ DailyProblems/everyday.js
@@ -0,0 +1,8 @@
+//Reverse a string
+function reverse(str){
+    return str.split('').reverse().join('');
+      
+    }
+    console.log(reverse("cat"))
+    console.log(reverse("tictactoe"))
+    console.log(reverse("hello world"))
