716da8206d8226e88f64a15074dca2041480fe14
divbeech <dyadav1006@gmail.com>
2021-06-23 12:59:32 -0400

    DB

--- DailyProblems/everyday.js
+++ DailyProblems/everyday.js
@@ -6,3 +6,15 @@ function reverse(str){
     console.log(reverse("cat"))
     console.log(reverse("tictactoe"))
     console.log(reverse("hello world"))
+
+//Palindrome
+var isPalindrome = function(s) {
+    let word = s.split('');
+    let reverse = word.reverse();
+    if (word === reverse){
+        return true
+    }
+    else{
+        return false
+    }
+};
